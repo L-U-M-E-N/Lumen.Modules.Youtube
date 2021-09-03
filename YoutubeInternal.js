087fde51ed5640ea19f94a870b1c0806ce1ece86
Elanis <elanis@hotmail.com>
2021-09-03 18:11:33 +0200

    Compatibility with lumen-server

---
+++ YoutubeInternal.js
@@ -0,0 +1,100 @@
+const fetch = require('node-fetch');
+
+const iso8601DurationRegex = /(-)?P(?:([.,\d]+)Y)?(?:([.,\d]+)M)?(?:([.,\d]+)W)?(?:([.,\d]+)D)?T(?:([.,\d]+)H)?(?:([.,\d]+)M)?(?:([.,\d]+)S)?/;
+function parseISO8601Duration(iso8601Duration) {
+	const matches = iso8601Duration.match(iso8601DurationRegex);
+
+	return {
+		sign: matches[1] === undefined ? '+' : '-',
+		years: matches[2] === undefined ? 0 : parseInt(matches[2]),
+		months: matches[3] === undefined ? 0 : parseInt(matches[3]),
+		weeks: matches[4] === undefined ? 0 : parseInt(matches[4]),
+		days: matches[5] === undefined ? 0 : parseInt(matches[5]),
+		hours: matches[6] === undefined ? 0 : parseInt(matches[6]),
+		minutes: matches[7] === undefined ? 0 : parseInt(matches[7]),
+		seconds: matches[8] === undefined ? 0 : parseInt(matches[8])
+	};
+}
+
+module.exports = class YoutubeInternal {
+	static async getVideoData(videoId, API_KEY) {
+		const res = await fetch(
+			`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${API_KEY}`
+		);
+
+		return await res.json();
+	}
+
+	static async getVideosData(videosId, API_KEY) {
+		const res = await fetch(
+			`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videosId.join(',')}&key=${API_KEY}`
+		);
+
+		return await res.json();
+	}
+
+	static async getPlayListData(playlistId, API_KEY) {
+		const finalResult = [];
+		let obj = {};
+		do {
+			let res;
+
+			if(obj.nextPageToken) {
+				res = await fetch(
+					`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${playlistId}&key=${API_KEY}&pageToken=${obj.nextPageToken}`
+				);
+			} else {
+				res = await fetch(
+					`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${playlistId}&key=${API_KEY}`
+				);
+			}
+
+			obj = await res.json();
+
+			const videos = await YoutubeInternal.getVideosData(obj.items.map((elt) => elt.snippet.resourceId.videoId), API_KEY);
+			finalResult.push(...obj.items.map((elt) => {
+				elt.videoData = videos.items.filter((video) =>
+					video.id === elt.snippet.resourceId.videoId
+				)[0];
+
+				return elt;
+			}));
+		} while(obj.nextPageToken);
+
+		return finalResult;
+	}
+
+	static async processPlaylistForModule(PLAYLIST_ID, API_KEY) {
+		let videoCount = 0;
+		let totalDuration = { hours: 0, minutes: 0, seconds: 0 };
+
+		const playlistData = await YoutubeInternal.getPlayListData(PLAYLIST_ID, API_KEY);
+
+		console.log('Video count: ', playlistData.length);
+		totalDuration = playlistData.reduce((acc, item) => {
+			const duration = parseISO8601Duration(item.videoData.contentDetails.duration);
+
+			acc.hours += duration.days * 24;
+			acc.hours += duration.hours;
+			acc.minutes += duration.minutes;
+			acc.seconds += duration.seconds;
+
+			while(acc.seconds > 60) {
+				acc.seconds -= 60;
+				acc.minutes++;
+			}
+
+			while(acc.minutes > 60) {
+				acc.minutes -= 60;
+				acc.hours++;
+			}
+
+			return acc;
+		}, { hours: 0, minutes: 0, seconds: 0 });
+		console.log('Total duration: ', totalDuration.hours.toString().padStart(2, '0') + ':' + totalDuration.minutes.toString().padStart(2, '0') + ':' + totalDuration.seconds.toString().padStart(2, '0'));
+
+		videoCount = playlistData.length;
+
+		return { totalDuration, videoCount };
+	}
+};
---
+++ main.server.js
@@ -0,0 +1,33 @@
+const YoutubeInternal = require('./YoutubeInternal');
+
+module.exports = class Youtube {
+	static init() {
+		Youtube.update();
+
+		Youtube.interval = setInterval(Youtube.update, 60 * 60 * 1000); // Update every hour
+	}
+
+	static close() {
+		clearInterval(Youtube.interval);
+	}
+
+	static async update() {
+		const {
+			totalDuration,
+			videoCount
+		} = await YoutubeInternal.processPlaylistForModule(youtubeCfg.PLAYLIST_ID, youtubeCfg.API_KEY);
+
+		const [query, values] = Database.buildInsertQuery('yt_stats', {
+			date: new Date(),
+			duration: totalDuration.hours * 3600 + totalDuration.minutes * 60 + totalDuration.seconds,
+			amount: videoCount
+		});
+
+		Database.execQuery(
+			query,
+			values
+		);
+
+		log('Saved current Youtube playlist status', 'info');
+	}
+};
