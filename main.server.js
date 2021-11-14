ca061575d205dbabcddb2484a5c2955f3450ee53
Elanis <elanis@hotmail.com>
2021-11-14 17:21:45 +0100

    #1239 - Insert more often, but only if changed

--- main.server.js
+++ main.server.js
@@ -4,7 +4,7 @@ export default class Youtube {
 	static init() {
 		Youtube.update();
 
-		Youtube.interval = setInterval(Youtube.update, 60 * 60 * 1000); // Update every hour
+		Youtube.interval = setInterval(Youtube.update, 5 * 60 * 1000); // Update every 5 minutes
 	}
 
 	static close() {
@@ -17,6 +17,10 @@ export default class Youtube {
 			videoCount
 		} = await YoutubeInternal.processPlaylistForModule(youtubeCfg.PLAYLIST_ID, youtubeCfg.API_KEY);
 
+		if(totalDuration === Youtube.prevTotalDuration && Youtube.prevVideoCount === videoCount) {
+			return;
+		}
+
 		const [query, values] = Database.buildInsertQuery('yt_stats', {
 			date: new Date(),
 			duration: totalDuration.hours * 3600 + totalDuration.minutes * 60 + totalDuration.seconds,
@@ -27,6 +31,9 @@ export default class Youtube {
 			query,
 			values
 		);
+
+		Youtube.prevTotalDuration = totalDuration;
+		Youtube.prevVideoCount = videoCount;
 
 		log('Saved current Youtube playlist status', 'info');
 	}
