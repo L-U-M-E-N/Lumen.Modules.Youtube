ec8318c0b78554c0ee839c2ee1780182703046eb
Elanis <elanis@hotmail.com>
2022-05-02 21:00:08 +0200

    Prevent module from crashing if Youtube API doesn't answer well

--- main.server.js
+++ main.server.js
@@ -12,34 +12,38 @@ export default class Youtube {
 	}
 
 	static async update() {
-		const content = await YoutubeInternal.processPlaylistForModule(global.config.youtube.PLAYLIST_ID, global.config.youtube.API_KEY);
+		try {
+			const content = await YoutubeInternal.processPlaylistForModule(global.config.youtube.PLAYLIST_ID, global.config.youtube.API_KEY);
 
-		if(!content) { return; }
+			if(!content) { return; }
 
-		const {
-			totalDuration,
-			videoCount
-		} = content;
-		const duration = totalDuration.hours * 3600 + totalDuration.minutes * 60 + totalDuration.seconds;
+			const {
+				totalDuration,
+				videoCount
+			} = content;
+			const duration = totalDuration.hours * 3600 + totalDuration.minutes * 60 + totalDuration.seconds;
 
-		if(duration === Youtube.prevTotalDuration && Youtube.prevVideoCount === videoCount) {
-			return;
+			if(duration === Youtube.prevTotalDuration && Youtube.prevVideoCount === videoCount) {
+				return;
+			}
+
+			const [query, values] = Database.buildInsertQuery('yt_stats', {
+				date: new Date(),
+				duration,
+				amount: videoCount
+			});
+
+			Database.execQuery(
+				query,
+				values
+			);
+
+			Youtube.prevTotalDuration = duration;
+			Youtube.prevVideoCount = videoCount;
+
+			log('Saved current Youtube playlist status', 'info');
+		} catch(e) {
+			console.error(e);
 		}
-
-		const [query, values] = Database.buildInsertQuery('yt_stats', {
-			date: new Date(),
-			duration,
-			amount: videoCount
-		});
-
-		Database.execQuery(
-			query,
-			values
-		);
-
-		Youtube.prevTotalDuration = duration;
-		Youtube.prevVideoCount = videoCount;
-
-		log('Saved current Youtube playlist status', 'info');
 	}
 }
